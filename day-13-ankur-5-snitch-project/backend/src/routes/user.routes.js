2ec2c7a4d50f8d5102ada538c2d689c54a6d1aea
Harsh Upadhyay <upadhyayharsh147@gmail.com>
2026-09-24 13:42:35 +0530

    created login validator

--- day-13-ankur-5-snitch-project/backend/src/routes/user.routes.js
+++ day-13-ankur-5-snitch-project/backend/src/routes/user.routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { getMe, login, refresh, register } from "../controllers/auth.controller.js";
 import { registerValidator } from "../validators/registerValidator.js";
 import { authenticateMiddleware } from "../middleware/auth.middleware.js";
+import { loginValidator } from "../validators/loginValidator.js";
 
-const router = Router()
+const router = Router() 
 
 
 router.post("/register",registerValidator, register)
-router.post('/login',login)
+router.post('/login',loginValidator,login)
 router.post('/refresh',refresh)
 router.get('/getMe',authenticateMiddleware,getMe)
 
